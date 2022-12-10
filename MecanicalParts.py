8f8b68c999a4c5a4de97ec743d964eb9825602f7
Viktor92 <larionov92spb@gmail.com>
2022-12-10 22:26:23 +0300

    fix problem whit button click

--- MecanicalParts.py
+++ MecanicalParts.py
@@ -5,7 +5,7 @@ class Button():
     def __init__(self, sf, img, width, height, x, y):
         self.sf = sf
         self.origin = pygame.image.load(img)
-        self.img = pygame.transform.scale(self.origin, (40, 10))
+        self.img = pygame.transform.scale(self.origin, (width, height))
         self.width = width
         self.height = height
         self.x = x
@@ -15,14 +15,9 @@ class Button():
         self.sf.blit(self.img, (self.x, self.y))
         pygame.display.flip()
 
-    def run(self):
-        a = self.x + self.width
-        b = self.y + self.height
-        ev = pygame.event.get()
-        for event in ev:
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                if pygame.mouse.get_pos() >= (self.x, self.y):
-                    print('click')
+    def check_click(self, mouse_pos):
+        mx, my = mouse_pos
+        return (self.x <= mx <= self.x + self.width) and (self.y <= my <= self.y + self.height)
 
     def resize(self, widthIn, heightIn):
         self.img = pygame.transform.scale(self.origin, (widthIn, heightIn))
@@ -40,10 +35,12 @@ pygame.init()
 screen = pygame.display.set_mode((500, 500))
 color = (255, 0, 0)
 button = Button(screen, "pixilart-drawing.png", 250, 250, 20, 20)
-button.run()
 while True:
     button.draw()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if button.check_click(event.pos):
+                print('Нажали на кнопку')
 
